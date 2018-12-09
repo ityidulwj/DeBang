9f47ff4878836d723e16651e882e9c16032c1945
lwj <itheima_lwj@163.com>
2018-12-09 19:03:35 +0800

    2

--- src/main/java/com/yidu/test/TestDemo.java
+++ src/main/java/com/yidu/test/TestDemo.java
@@ -1,9 +1,7 @@
 package com.yidu.test;
 
-import org.apache.log4j.chainsaw.Main;
-
 public class TestDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World");
 	}
 }
