908a212b89d67de1f4e5df50073c679cb717cc12
lwj <itheima_lwj@163.com>
2018-12-09 19:08:24 +0800

    测试

--- src/main/java/com/yidu/test/TestDemo.java
+++ src/main/java/com/yidu/test/TestDemo.java
@@ -1,14 +1,15 @@
 package com.yidu.test;
+
 /**
- * <p>Title:TestDemo</p>
+ * <p>Title：TestDemo</p>
  * <p>Description: 测试类</P>	
  * <p>Company:www.yiduedu.com</p>
  * @author 	LWJ
- * @date	2018-12-09 下午7:03:55
+ * @date	2018-12-09 下午7:07:51
  * @version 1.0
  */
 public class TestDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 }
