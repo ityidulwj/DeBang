c83fd5b694ba7dccf23c105f6f1245e272a803d0
Administrator <Administrator@X08NEUIF9SO15PS>
2018-12-27 18:13:03 +0800

    刘润虎

--- src/main/java/com/yidu/test/TestDemo.java
+++ src/main/java/com/yidu/test/TestDemo.java
@@ -1,15 +1,42 @@
 package com.yidu.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yidu.lrh.bean.Delivery;
+import com.yidu.lrh.dao.DeliveryMapper;
+
 /**
+ * 
  * <p>Title：TestDemo</p>
- * <p>Description: 测试类</P>	
- * <p>Company:www.yiduedu.com</p>
- * @author 	LWJ
- * @date	2018-12-09 下午7:07:51
+ * <p>Description：</p>
+ * <p>Company：www.yiduedu.com</p>
+ * @author LWJ
+ * @date 2018-12-11 上午9:37:24
  * @version 1.0
+ * 
  */
 public class TestDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		/*SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(new Date()));*/
+		new TestDemo().demo();
 	}
+	
+	public void demo() {
+		@SuppressWarnings("resource")
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//StorageMapper必须小写
+		DeliveryMapper bean = ac.getBean("deliveryMapper",DeliveryMapper.class);
+		Map<String, Object> maps=new HashMap<String, Object>();
+		maps.put("page", (1-1)*5);
+		maps.put("row", 5);
+		for (Delivery Sto : bean.selectLimit(maps)) {
+			System.out.println(Sto.toString());
+		}
+	}
+	
 }
